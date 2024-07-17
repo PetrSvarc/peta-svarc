9a494ebd2a7923ef16f90e576600a505a75f709f
petrsvarc <petr.svarc@nice.com>
2024-07-17 22:41:42 +0200

    Init project

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script setup lang="ts">
+  import { useQuery } from '@tanstack/vue-query'
+
+  const fetcher = async () =>
+      await fetch('https://frontapi.praguebest.cz/api-products/products/iphone-xr-256gb-zluta').then((response) =>
+          response.json(),
+      )
+
+  const { data, suspense } = useQuery({ queryKey: ['test'], queryFn: fetcher })
+
+  await suspense()
+</script>
